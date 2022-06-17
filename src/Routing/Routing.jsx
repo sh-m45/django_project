feef02876a4fd2a14a72166e00299495235efdea
Mohamed Eldefrawy <mohamed.ahmed.aly.d@gmail.com>
2022-06-17 21:41:26 +0200

    Fix react prime issue

--- src/Routing/Routing.jsx
+++ src/Routing/Routing.jsx
@@ -1,13 +1,13 @@
 import React, {useEffect, useState} from "react";
-import {NavLink, Route, Routes, useNavigate} from "react-router-dom";
+import {Route, Routes, useNavigate} from "react-router-dom";
 
 import Profile from "../components/Profile/Profile";
-import Jobs from "../components/Jobs/Jobs";
 import Login from "../components/Login/Login";
 import Register from "../components/Register/Register";
 import Header from "./Header";
-import {render} from "@testing-library/react";
 import Notification from "../components/notifications/Notifications";
+import JobsList from "../components/Jobs/JobsList";
+import JobDetails from "../components/Jobs/JobsDetails";
 
 export default function Routing() {
     const [userData, setUserData] = useState(null);
@@ -31,8 +31,9 @@ export default function Routing() {
         <>
             <Header userData={userData} logOut={logOut}/>
             <Routes>
-                <Route path="/" exact element={<Jobs/>}/>
-                <Route path="/jobs" element={<Jobs/>}/>
+                <Route path="/" exact element={<JobsList/>}/>
+                <Route path="/jobs" element={<JobsList/>}/>
+                <Route path="/jobs/:id" element={<JobDetails/>}/>‏
                 <Route path="/profile" element={<Profile/>}/>
                 <Route path="/login" element={<Login getUserData={getUserData}/>}/>
                 <Route path="/register" element={<Register/>}/>
--- src/index.js
+++ src/index.js
@@ -1,32 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
-import { NavLink } from 'react-router-dom'
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";
 import Routing from './Routing/Routing';
-import Header from './Routing/Header';
-import Profile from './components/Profile/Profile';
-import Jobs from './components/Jobs/Jobs';
-import Register from './components/Register/Register';
-import Login from './components/Login/Login';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 //import '../node_modules/@fortawesome/free-solid-svg-icons/faSpinner'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    
-    <BrowserRouter>
-    <Routing/>
-    
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        {/* <App /> */}
+
+        <BrowserRouter>
+            <Routing/>
+
+        </BrowserRouter>
+    </React.StrictMode>
 );
 
 
